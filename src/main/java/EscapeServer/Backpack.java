204e7f563deea6e581b0c318312da8718129b1b2
Mauneel123 <48901093+Mauneel123@users.noreply.github.com>
2019-03-25 16:33:17 -0400

    Update Backpack.java

--- src/main/java/EscapeServer/Backpack.java
+++ src/main/java/EscapeServer/Backpack.java
@@ -2,7 +2,7 @@ package EscapeServer;
 
 import java.util.ArrayList;
 import java.util.List;
-
+/*Class which stores all the information regarding backpacks*/
 class Backpack {
 
     private String userID;
@@ -14,19 +14,21 @@ class Backpack {
         this.slotAmount = slotAmount;
         this.equipmentList = new ArrayList<Integer>();
     }
-
+    //function to remove equipment from the backpack
     Integer removeEquipment(int index){
         Integer ret = this.equipmentList.get(index);
         this.equipmentList.remove(index);
         return ret;
     }
-
+//function to add equipment from the backpack
     Boolean addEquipment(Integer equipmentReference){
         if(this.equipmentList.size() < this.slotAmount){
             this.equipmentList.add(equipmentReference);
             return true;
         }else return false;
     }
+    
+    //function to check if the backpack is full
     Boolean checkNotFull(){
         if(this.equipmentList.size() < this.slotAmount) return true;
         return false;
@@ -35,7 +37,8 @@ class Backpack {
     String getUserID() {
         return this.userID;
     }
-
+    
+    // backpack is reset to zero items
     void reset(){
         this.slotAmount = 4;
         this.equipmentList.clear();
@@ -44,7 +47,7 @@ class Backpack {
     Integer getSlotAmount() {
         return this.slotAmount;
     }
-
+    //generates the list of equipment
     List<Integer> getEquipmentList() {
         return this.equipmentList;
     }
